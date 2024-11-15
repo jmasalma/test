05e7a0d6d87148321d44cab30a42a9b13f215f2a
jmasalma <49733702+jmasalma@users.noreply.github.com>
2024-11-15 18:22:22 -0500

    Create config.ts

---
+++ docs/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQxSwoPXbRv_6ChZDLBfj5E6Uck6Yfl94",
+  authDomain: "masalma-d9013.firebaseapp.com",
+  projectId: "masalma-d9013",
+  storageBucket: "masalma-d9013.firebasestorage.app",
+  messagingSenderId: "338314069643",
+  appId: "1:338314069643:web:b77c588957b4e93649b68e",
+  measurementId: "G-CQQVD3TE5N"
+};
